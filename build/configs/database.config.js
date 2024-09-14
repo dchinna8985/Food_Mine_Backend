c99380ed296373298bf666c74f60d6c67fbd1131
dchinna8985 <122163431+dchinna8985@users.noreply.github.com>
2024-09-14 22:54:13 +0530

    Delete build directory

--- build/configs/database.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dbConnect = void 0;
-const mongoose_1 = require("mongoose");
-const dbConnect = () => {
-    (0, mongoose_1.connect)(process.env.MONGO_URI, {}).then(() => console.log("Connect Successfully"), (error) => console.log(error));
-};
-exports.dbConnect = dbConnect;
-//# sourceMappingURL=database.config.js.map
